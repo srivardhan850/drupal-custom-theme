a3a1b98f1e3d447cbcf3f404ba05068f5d08e0d2
srivardhan <155811858+srivardhan850@users.noreply.github.com>
2025-04-09 21:01:55 +0530

    Add files via upload

---
+++ theme-settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Theme settings file for Custom Theme.
+ */
+
+function custom_theme_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Add custom theme settings here.
+}
